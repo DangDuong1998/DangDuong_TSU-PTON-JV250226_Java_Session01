9e4cd344fef6d7d80baae317b3d8998e4d791a87
DangDuong1998 <mvstarkfinal.ultra@gmail.com>
2025-05-14 21:20:03 +0900

    [Bài tập] Hiển thị tên và tuổi ra màn hình

---
+++ bt_02/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s", "Đặng Thái Dương", 27, "Hà Nội");
+
+    }
+}
